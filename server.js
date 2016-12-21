5f0f042d60676f70063d11a6d2820207d73708bd
Madison Kerndt <madison.kerndt@gmail.com>
2016-12-21 10:20:02 -0700

    count votes

--- server.js
+++ server.js
@@ -19,4 +19,48 @@ var server = http.createServer(app)
   console.log('Listening on port' + port + '.');
 });
 
+var socketIo = require('socket.io');
+var io = socketIo(server);
+
+// set up event listner for connection event on the server
+io.on('connection', function (socket) {
+  console.log('A user has connected.', io.engine.clientsCount);
+
+  io.sockets.emit('userConnection', io.engine.clientsCount);
+
+  socket.emit('statusMessage', 'You have connected.');
+
+  socket.on('message', function (channel, message) {
+    if (channel === 'voteCast') {
+      votes[socket.id] = message;
+      console.log(votes);
+      socket.emit('voteCount', countVotes(votes));
+    }
+  });
+
+  socket.on('disconnect', function () {
+    console.log('A user has disconnected.', io.engine.clientsCount);
+    delete votes[socket.id];
+    socket.emit('voteCount', countVotes(votes));
+    io.sockets.emit('userConnection', io.engine.clientsCount);
+  });
+});
+
+// write a better version using lodash
+function countVotes(votes) {
+  var voteCount = {
+    A: 0,
+    B: 0,
+    C: 0,
+    D: 0
+  };
+  for (var vote in votes){
+    voteCount[votes[vote]]++;
+  }
+  return voteCount;
+}
+
+var votes = {};
+
+
 module.exports = server;
